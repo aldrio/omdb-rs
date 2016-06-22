789705d7365809b73c1bccb88c5b90d8c2a3a3e0
Brandon Aldrich <brandon@aldr.io>
2016-06-22 14:59:54 -0600

    Fixed tabs

--- build.rs
+++ build.rs
@@ -5,11 +5,11 @@ use std::path::Path;
 
 pub fn main() {
 
-    let out_dir = env::var_os("OUT_DIR").unwrap();
+	let out_dir = env::var_os("OUT_DIR").unwrap();
 
 	// Generate deserialization code for models
-    let src = Path::new("src/query/model.rs.in");
-    let dst = Path::new(&out_dir).join("model.rs");
+	let src = Path::new("src/query/model.rs.in");
+	let dst = Path::new(&out_dir).join("model.rs");
 
-    serde_codegen::expand(&src, &dst).unwrap();
+	serde_codegen::expand(&src, &dst).unwrap();
 }
